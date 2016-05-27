00c6d1ce9988c6bf492b462194883f87b9e02396
Adam McMaster <adam@zooniverse.org>
2016-05-27 12:01:48 +0100

    Merge Joey's plotting code

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import csv
+import numpy
 import os
 import progressbar
 import requests
 import sys
 
 from matplotlib import pyplot
+from matplotlib.ticker import ScalarFormatter
+
+# Amount of time before/after TCE midtransit
+BUFFERTIMES = (0.5, 1.5, 5)
 
 OUTPATH = os.environ.get('OUTPATH', os.path.join('/', 'out'))
 
@@ -20,7 +25,7 @@ with open(INPUT_FILE_LIST) as input_files_f:
     input_headers = input_rows.next()
     bar = progressbar.ProgressBar(redirect_stdout=True)
 
-    for input_row in bar(input_rows):
+    for input_row in bar(list(input_rows)):
         input_data = dict(zip(input_headers, input_row))
 
         lightcurve = requests.get(input_data['datalocation'])
@@ -33,6 +38,7 @@ with open(INPUT_FILE_LIST) as input_files_f:
 
         userxmin = float(input_data['userxmin'])
         userxmax = float(input_data['userxmax'])
+        userxmid = float(input_data['userxmid'])
 
         output_filename = input_data['datalocation'].split('/')[-1]
         output_filename = output_filename.replace('.json', '')
@@ -42,20 +48,84 @@ with open(INPUT_FILE_LIST) as input_files_f:
             userxmax
         )
 
-        sliced_lightcurve = {
-            'x': [],
-            'y': [],
-        }
+        fig, axes = pyplot.subplots(3, 1)
+        fig.set_size_inches(10, 15.0)
+        fig.subplots_adjust(right=0.98, top=0.98, left=0.1, bottom=0.04)
 
-        for i, x in enumerate(lightcurve['x']):
-            # Assumes the data is sorted by x
-            if x < userxmin:
+        yaxisformatter = ScalarFormatter(useOffset=False)
+
+        alltime = numpy.array(lightcurve['x'])
+        allflux = numpy.array(lightcurve['y'])
+
+        for buffertime, ax in zip(BUFFERTIMES, axes):
+            # Cutting out region of transit +/- buffertime
+            time = 24.0 * (
+                alltime[
+                    numpy.where(
+                        (alltime > (userxmin - buffertime)) &
+                        (alltime < (userxmax + buffertime))
+                    )
+                ] - userxmid
+            )
+
+            if len(time) == 0:
                 continue
-            if x > userxmax:
-                break
-            sliced_lightcurve['x'].append(x)
-            sliced_lightcurve['y'].append(lightcurve['y'][i])
 
-        pyplot.plot(sliced_lightcurve['x'], sliced_lightcurve['y'])
-        pyplot.savefig(os.path.join(OUTPATH, output_filename))
-        pyplot.close()
+            origflux = allflux[
+                numpy.where(
+                    (alltime > (userxmin - buffertime)) &
+                    (alltime < (userxmax + buffertime))
+                )
+            ]
+
+            # Normalizing fluxes to ~1.0
+            flux = origflux / numpy.nanmedian(origflux)
+            ax.plot(time, flux, marker='o', color='b', ls='-')
+
+            # Plotting extra edge space in x-axis
+            xlim = numpy.nanmax([
+                abs(numpy.nanmin(time)), abs(numpy.nanmax(time))
+            ]) + 2.0
+            ax.set_xlim(-xlim, xlim)
+
+            # Plotting extra edge space in y-axis
+            ydiff = numpy.nanmax(flux) - numpy.nanmin(flux)
+            ydifffactor = 0.05
+            ylims = (
+                numpy.nanmin(flux) - ydiff * ydifffactor,
+                numpy.nanmax(flux) + ydiff * ydifffactor
+            )
+            ax.set_ylim(ylims)
+            ax.yaxis.set_major_formatter(yaxisformatter)
+
+            # Drawing vertical lines around transit
+            normuserxmin = 24.0 * (userxmin - userxmid)
+            normuserxmax = 24.0 * (userxmax - userxmid)
+
+            ax.plot(
+                [0, 0],
+                ylims,
+                marker='None',
+                color='k',
+                ls='--'
+            )
+            ax.plot(
+                [normuserxmin, normuserxmin],
+                ylims,
+                marker='None',
+                color='k',
+                ls='-'
+            )
+            ax.plot(
+                [normuserxmax, normuserxmax],
+                ylims,
+                marker='None',
+                color='k',
+                ls='-'
+            )
+            ax.set_ylabel('Normalized flux')
+            ax.set_xlabel('Time (hours from midtransit)')
+
+        # Writing figures or plotting
+        fig.savefig(os.path.join(OUTPATH, output_filename))
+        pyplot.close(fig)
